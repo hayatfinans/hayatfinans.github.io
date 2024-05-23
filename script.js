5548e39b6fa4e78ccb1bc17532821fdc56c290d4
hayatfinans <hayatfinans@protonmail.com>
2024-05-23 13:21:53 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function shortenLink() {
+    var longUrl = document.getElementById('longUrl').value;
+    if (longUrl) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'dhosting.yzz.me/shorten.php', true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('shortenedUrl').innerHTML = 'Kısaltılmış Bağlantı: ' + xhr.responseText;
+            }
+        };
+        xhr.send('url=' + encodeURIComponent(longUrl));
+    }
+}
