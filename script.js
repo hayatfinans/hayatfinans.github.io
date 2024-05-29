e063e2e31d5bf9df51cdbf9a83d349e6ec02f557
hayatfinans <hayatfinans@protonmail.com>
2024-05-29 23:19:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var dragDiv = document.getElementById('drag-div');
-
 var offsetX, offsetY;
 
-dragDiv.addEventListener('dragstart', function(event) {
-    offsetX = event.clientX - dragDiv.getBoundingClientRect().left;
-    offsetY = event.clientY - dragDiv.getBoundingClientRect().top;
+dragDiv.addEventListener('touchstart', function(event) {
+    offsetX = event.touches[0].clientX - dragDiv.getBoundingClientRect().left;
+    offsetY = event.touches[0].clientY - dragDiv.getBoundingClientRect().top;
 });
 
-dragDiv.addEventListener('drag', function(event) {
-    event.preventDefault(); // Fare olaylarının varsayılan davranışını engelle
-    dragDiv.style.left = (event.clientX - offsetX) + 'px';
-    dragDiv.style.top = (event.clientY - offsetY) + 'px';
+dragDiv.addEventListener('touchmove', function(event) {
+    event.preventDefault(); // Dokunmatik olayların varsayılan davranışını engelle
+    dragDiv.style.left = (event.touches[0].clientX - offsetX) + 'px';
+    dragDiv.style.top = (event.touches[0].clientY - offsetY) + 'px';
 });
